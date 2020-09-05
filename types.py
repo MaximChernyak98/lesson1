741db204406982a9d6427aff14f69618ac2ff1ee
mecher_l_home <maximchernyak98@gmail.com>
2020-09-05 18:15:08 +0300

    добавил types.py/ add types.py

---
+++ types.py
@@ -0,0 +1,28 @@
+# Задача 1
+a = 2
+b = 0.5
+print(a+b)
+
+# Задача 2
+name = 'МАКСИМ'
+print(name)
+
+# Задача 3
+#v = input('Введите число от 1 до 10: ')
+# print(int(v)+10)
+
+# Задача 4
+#name = input('Введите Ваше имя: ')
+#print(f'Привет, {name}! Как дела?')
+
+# Задача 5
+print(float('1'))
+
+try:
+    print(int('2.5'))
+except ValueError:
+    print('неверный тип данных')  # ошибка по типу
+
+print(bool(1))
+print(bool(''))
+print(bool(0))
